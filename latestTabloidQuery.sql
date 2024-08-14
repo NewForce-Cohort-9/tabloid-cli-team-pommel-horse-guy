b325b4c22fe752c8a57e5f0aa93e526f30317383
LJ White <theljwhite@gmail.com>
2024-08-14 11:31:43 -0400

    finished CRUD for blogs except BlogDetails

---
+++ latestTabloidQuery.sql
@@ -0,0 +1,7 @@
+﻿SELECT * FROM Tag;
+
+SELECT b.*, t.Id AS TagId, t.Name AS TagName, bt.Id AS BlogTagId, bt.BlogId, bt.TagId 
+FROM Blog b
+JOIN BlogTag bt ON bt.Id = b.Id
+JOIN Tag t ON t.Id = bt.TagId
+WHERE b.Id = bt.BlogId; 
